f1366f17e2ffceaed4dfa279f15483dc4b4d28b4
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-10 12:07:02 +0000

    Create poems model

--- poems/models.py
+++ poems/models.py
@@ -1,3 +1,26 @@
+"""Database Model for Poems App"""
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Poem(models.Model):
+    """Poems Model"""
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poem_posts")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.IntegerField(blank=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
