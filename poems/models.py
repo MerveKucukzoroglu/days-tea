d4d6561885e49fa38cebea742eda0fb4433148df
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-10 14:25:18 +0000

    Add likes to post model and create comments model

--- poems/models.py
+++ poems/models.py
@@ -6,7 +6,7 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
-class Poem(models.Model):
+class Post(models.Model):
     """Poems Model"""
     title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poem_posts")
@@ -17,10 +17,32 @@ class Poem(models.Model):
     excerpt = models.IntegerField(blank=True)
     slug = models.SlugField(max_length=200, unique=True)
     approved = models.BooleanField(default=False)
+    likes = models.ManyToManyField(User, related_name='poem_likes', blank=True)
 
     class Meta:
+        """created on field"""
         ordering = ['-created_on']
 
     def __str__(self):
+        """return self title"""
         return self.title
 
+    def number_of_likes(self):
+        """number of likes in a post"""
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    """Comments model class"""
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """meta class for comments"""
+        ordering = ['created_on']
+
+    def __str__(self):
+        """comment str return"""
+        return f"Comment {self.body} by {self.name}"
