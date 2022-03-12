dba56a24e612c6f8b7533021a15668a10af4f53a
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-12 16:16:32 +0000

    Create skeleton login/logout pages

--- poems/views.py
+++ poems/views.py
@@ -1,6 +1,6 @@
 """Views file for the Poems"""
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -10,3 +10,25 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class PoemDetails(View):
+    """Open and view a post"""
+    def get(self, request, slug, *args, **kwargs):
+        """getting relevent post information"""
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "poem_details.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
