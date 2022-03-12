79597a380e69f3ad1e3155e0ed9cdd1fead0747e
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-12 18:33:46 +0000

    Create post like/unlike function

--- poems/urls.py
+++ poems/urls.py
@@ -6,4 +6,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PoemDetails.as_view(), name='poem_details'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='poem_likes'),
 ]
--- poems/views.py
+++ poems/views.py
@@ -1,6 +1,7 @@
 """Views file for the Poems"""
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -40,7 +41,7 @@ class PoemDetails(View):
         """posting comments"""
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.order_by('created_on')
+        comments = post.comments.order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -49,7 +50,7 @@ class PoemDetails(View):
 
         if comment_form.is_valid():
             comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=True)
+            comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
         else:
@@ -66,3 +67,16 @@ class PoemDetails(View):
                 "comment_form": CommentForm()
             },
         )
+
+class PostLike(View):
+    """class view for like and unlike a post"""
+    def post(self, request, slug):
+        """function to like/unlike"""
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('poem_details', args=[slug]))
