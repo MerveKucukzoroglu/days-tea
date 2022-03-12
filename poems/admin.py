703875187e9ae67a253f36d95d7315ca1a878147
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-12 11:23:06 +0000

    Create template files skeleton structures

--- poems/admin.py
+++ poems/admin.py
@@ -1,12 +1,22 @@
 """admin panel"""
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     """summernote model for admin"""
+    list_display = ('title', 'slug', 'approved', 'status', 'created_on')
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('approved', 'created_on')
-    summernote_fields = ('content')
+    summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Comment admin panel"""
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('name', 'created_on')
+    search_fields = ('name', 'email', 'body')
--- poems/models.py
+++ poems/models.py
@@ -10,13 +10,15 @@ class Post(models.Model):
     """Poems Model"""
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poem_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="poem_posts")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     approved = models.BooleanField(default=False)
+    status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='poem_likes', blank=True)
 
     class Meta:
