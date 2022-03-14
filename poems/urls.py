ba7732c626cf61ffd2afe5f57a9be5d4f61d8e05
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-14 08:43:18 +0000

    Fix the PostList view, and see the poem list content

--- poems/urls.py
+++ poems/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('poem_list', views.PostList.as_view(), name='poem_list'),
+    path('poem_list/', views.PostList.as_view(), name='poem_list'),
     path('<slug:slug>/', views.PoemDetails.as_view(), name='poem_details'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='poem_likes'),
 ]
--- poems/views.py
+++ poems/views.py
@@ -9,25 +9,12 @@ def index(request):
     return render(request, 'index.html')
 
 
-class PostList(View):
+class PostList(generic.ListView):
     """Site pagination and order of poems"""
-    def get(self, request, *args, **kwargs):
-        """postlist"""
-        model = Post
-        queryset = Post.objects.filter(status=1).order_by('-created_on')
-        post = get_object_or_404(queryset)
-        template_name = 'poem_list.html'
-        paginate_by = 6
-
-        return render(
-            request, 'poem_list.html',
-            {
-                "model": model,
-                "post": post,
-                "template_name": template_name,
-                "paginate_by": paginate_by
-            },
-        )
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'poem_list.html'
+    paginate_by = 6
 
 
 class PoemDetails(View):
