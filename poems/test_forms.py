277ec4f7b628eed13fc0426c145a4acf7c1fd56a
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-22 13:36:16 +0000

    Test Comments form to include body and is a required field

--- poems/test_forms.py
+++ poems/test_forms.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .forms import PoemForm
+from .forms import PoemForm, CommentForm
 
 
 class TestPoemForm(TestCase):
@@ -21,3 +21,16 @@ class TestPoemForm(TestCase):
         self.assertEqual(
             form.Meta.fields, ('title', 'content', 'excerpt', 'featured_image')
             )
+
+
+class TestCommentForm(TestCase):
+
+    def test_post_title_is_required(self):
+        form = CommentForm(({'body': ''}))
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = CommentForm()
+        self.assertEqual(form.Meta.fields, ('body',))
