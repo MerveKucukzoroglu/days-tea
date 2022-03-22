c539159ff16a72958426e8ed46fdc0af2db8aee6
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-22 12:26:39 +0000

    Write test to test PoemForm title requirement

--- poems/test_forms.py
+++ poems/test_forms.py
@@ -1,1 +1,11 @@
 from django.test import TestCase
+from .forms import PoemForm
+
+
+class TestPoemForm(TestCase):
+
+    def test_post_title_is_required(self):
+        form = PoemForm(({'title': ''}))
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
