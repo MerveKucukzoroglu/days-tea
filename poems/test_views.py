ce2e0e951cdc23edee9d900aa15f21bdea9f62a1
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-22 15:03:40 +0000

    Test views for poem post list page

--- poems/test_views.py
+++ poems/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Post
 
 
 class TestViews(TestCase):
@@ -7,3 +8,8 @@ class TestViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+    def test__get_post_list_page(self):
+        response = self.client.get('/poem_list/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'poem_list.html')
