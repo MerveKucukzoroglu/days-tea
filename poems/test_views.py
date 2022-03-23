760f7ba8821f43625d8af1ad054919b046f90857
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-23 08:29:00 +0000

    Create profile views test

--- poems/test_views.py
+++ poems/test_views.py
@@ -2,14 +2,22 @@ from django.test import TestCase
 from .models import Post
 
 
-class TestViews(TestCase):
-
+class TestIndexViews(TestCase):
     def test_get_index_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
-    def test__get_post_list_page(self):
+
+class TestPostListViews(TestCase):
+    def test_get_post_list_page(self):
         response = self.client.get('/poem_list/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'poem_list.html')
+
+
+class TestProfileViews(TestCase):
+    def test_profile_page(self):
+        response = self.client.get('/profile')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profile.html')
