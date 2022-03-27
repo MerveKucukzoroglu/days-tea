517aedb02eccf85ac35e8365d3e551a308923b8c
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-27 10:49:20 +0000

    Add comments to test_models.py

--- poems/test_models.py
+++ poems/test_models.py
@@ -1,7 +1,9 @@
+"""Unit Testing for Models"""
 from django.test import TestCase
 from .models import Post
 
 
 class TestModels(TestCase):
+    """Unit Testing models has featured image"""
     def test_has_featured_image(self):
         self.assertTrue(Post.featured_image)
