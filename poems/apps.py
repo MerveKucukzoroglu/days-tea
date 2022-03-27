ee80deba70ea48e51f0dea55b2ebb97256beb61f
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-03-27 10:51:33 +0000

    Add comments to apps page

--- poems/apps.py
+++ poems/apps.py
@@ -1,6 +1,8 @@
+"""Apps"""
 from django.apps import AppConfig
 
 
 class PoemsConfig(AppConfig):
+    """Configuring the Poems App"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'poems'
